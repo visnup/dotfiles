aebd1d4066b696cdfb93536d7147819f7cc3d664
Visnu Pitiyanuvath <visnup@gmail.com>
2017-07-12 01:52:40 -0700

    use website to seed a month of lines

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,7 +1,4 @@
 function fish_greeting
-  set r (random 1 1000)
-  set word (curl -s https://unpkg.com/common-english-words@0.0.6/1000-common-words.txt | awk "NR==$r {print \$2}")
-  curl -s "https://harpers.org/findings?search=$word" | pup '.findresult .data p text{}' > /tmp/findings.txt
-  set r (random 1 (wc -l /tmp/findings.txt | awk '{print $1}'))
-  awk "NR==$r" /tmp/findings.txt
+  set line (random 1 (cat ~/.motd | wc -l))
+  awk "NR==$line" ~/.motd | fold -s
 end
