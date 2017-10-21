19e8731db271d828445ca63fb61feee879440476
Visnu Pitiyanuvath <visnup@gmail.com>
2017-10-21 16:07:31 -0700

    cleaner docker-machine eval

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,9 @@
 if which rbenv >/dev/null
   status --is-interactive; and . (rbenv init -|psub)
 end
+
+if which docker-machine >/dev/null
+  if docker-machine env 2>/dev/null
+    eval (docker-machine env)
+  end
+end
