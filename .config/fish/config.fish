9665c512213d23e87951890beccccc0c53733205
Visnu Pitiyanuvath <visnup@gmail.com>
2018-10-28 15:13:13 -0700

    using docker over rbenv

--- .config/fish/config.fish
+++
@@ -1,9 +0,0 @@
-if which rbenv >/dev/null
-  status --is-interactive; and . (rbenv init -|psub)
-end
-
-if which docker-machine >/dev/null
-  if docker-machine env 2>/dev/null
-    eval (docker-machine env)
-  end
-end
