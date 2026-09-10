f6e14bc36574edad1632aa4332796dca1c48fc29
Visnu Pitiyanuvath <visnup@gmail.com>
2026-09-10 13:44:23 -0700

    about to upgrade

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,3 +7,13 @@ end
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+
+set -gx JAVA_HOME /opt/homebrew/opt/openjdk@21/libexec/openjdk.jdk/Contents/Home
+
+# uv tool shims: frida, frida-ps, frida-trace, ...
+fish_add_path $HOME/.local/bin
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /Users/visnup/.lmstudio/bin
+# End of LM Studio CLI section
+
